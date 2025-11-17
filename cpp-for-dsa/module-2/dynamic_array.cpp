d14e67467442db5b70efaf006bd768226d8282e1
ahadahamedakash <a.ahad2232@gmail.com>
2025-11-18 00:04:18 +0600

    Dynamic array, variable and deleting of dynamic array

---
+++ cpp-for-dsa/module-2/dynamic_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int a[5];
+    int *a = new int[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
