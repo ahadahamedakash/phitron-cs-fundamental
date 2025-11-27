1a1f96ce8b766f21a31ee68f173b2b4d1765520a
ahadahamedakash <a.ahad2232@gmail.com>
2025-11-27 22:34:48 +0600

    String operation in cpp p2

--- cpp-for-dsa/module-5/string.cpp
+++ cpp-for-dsa/module-5/string.cpp
@@ -19,6 +19,18 @@ int main()
 
     // s.pop_back(); // will remove last character
 
+    // s = "New World"; // will assign new string
+
+    // s.assign("New");
+
+    // s.erase(4); // will remove rest after the 4th index
+
+    // s.erase(2, 2); // will remove from index 2 and 2 character
+
+    // s.replace(5, 0, " Bangladesh");
+
+    s.insert(5, " Bangladesh");
+
     cout << s << endl;
 
     return 0;
