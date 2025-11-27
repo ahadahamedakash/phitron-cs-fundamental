7d4c0bffbe9ea45f5cba2c14036e0727190381e1
ahadahamedakash <a.ahad2232@gmail.com>
2025-11-27 22:27:01 +0600

    String operation in cpp

---
+++ cpp-for-dsa/module-5/string.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s = "Hello";
+
+    string s2 = "World";
+
+    // s += s2; // Hello World
+    // s.append(s2); // Hello World
+
+    // s.push_back('o');
+
+    // s[0] = 'h'; // will change n index character
+
+    // int size = s.size();
+
+    // s.pop_back(); // will remove last character
+
+    cout << s << endl;
+
+    return 0;
+}
