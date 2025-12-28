8972d86f80cffd1a35e2705cbf137661c7f4ac8c
ahadahamedakash <a.ahad2232@gmail.com>
2025-12-28 22:26:25 +0600

    Solved dsa ass problem 2

--- data-structure/ass-1/2.cpp
+++ data-structure/ass-1/2.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    vector<long long> v(n), sumArr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sumArr[0] = v[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        sumArr[i] = sumArr[i - 1] + v[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << sumArr[i] << " ";
+    }
 
     return 0;
 }
