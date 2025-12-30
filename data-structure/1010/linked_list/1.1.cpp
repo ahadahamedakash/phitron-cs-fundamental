eeb439d04acc5cd50776a2268fd6cbbf9706e01d
ahadahamedakash <a.ahad2232@gmail.com>
2025-12-30 13:53:39 +0600

    LL practice

---
+++ data-structure/1010/linked_list/1.1.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void print()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+
+            temp = temp->next;
+        }
+    }
+};
+
+int main()
+{
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.push_front(0);
+
+    ll.print();
+
+    return 0;
+}
