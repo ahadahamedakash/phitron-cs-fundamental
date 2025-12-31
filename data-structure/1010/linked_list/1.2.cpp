b812985ab1156ac961503283e8987444935ec641
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-01 01:34:38 +0600

    LL AC

--- data-structure/1010/linked_list/1.2.cpp
+++ data-structure/1010/linked_list/1.2.cpp
@@ -1,67 +1,85 @@
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-// using namespace std;
+using namespace std;
 
-// class Node
-// {
-// public:
-//     int data;
-//     Node *next;
+class Node
+{
+public:
+    int data;
+    Node *next;
 
-//     Node(int val)
-//     {
-//         data = val;
-//         next = NULL;
-//     }
-// };
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
 
-// class List
-// {
-//     Node *head;
-//     Node *tail;
+class List
+{
+    Node *head;
+    Node *tail;
 
-// public:
-//     List()
-//     {
-//         head = tail = NULL;
-//     }
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
 
-//     void push_front(int val)
-//     {
-//         Node *newNode = new Node(val);
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
 
-//         if (head == NULL)
-//         {
-//             head = newNode;
-//             tail = newNode;
-//         }
-//         else
-//         {
-//             head->next = head;
-//             head = newNode;
-//         }
-//     }
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
 
-//     void print()
-//     {
-//         Node *temp = head;
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
 
-//         while (temp != NULL)
-//         {
-//             cout << temp->data << " ";
-//             temp = temp->next;
-//         }
-//     }
-// };
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
 
-// int main()
-// {
-//     List ll;
+        tail = newNode;
+        }
 
-//     ll.push_front(1);
-//     ll.push_front(0);
+    void print()
+    {
+        Node *temp = head;
 
-//     ll.print();
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+};
 
-//     return 0;
-// }
+int main()
+{
+    List ll;
+
+    // ll.push_front(3);
+    // ll.push_front(2);
+    // ll.push_front(1);
+    ll.push_front(0);
+
+    ll.push_back(1);
+    // ll.push_back(2);
+
+    ll.print();
+
+    return 0;
+}
