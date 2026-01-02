febac84b8eca047a77e8e7d5e435b8a7e70ba4ee
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-02 13:39:00 +0600

    LL AC Practice

--- data-structure/1010/linked_list/1.1.cpp
+++ data-structure/1010/linked_list/1.1.cpp
@@ -42,6 +42,50 @@ public:
         }
     }
 
+    void push_back(int val)
+    {
+
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void pop_back()
+    {
+        if (head == NULL)
+            return;
+
+        Node *temp = head;
+
+        while (temp->next != tail)
+            temp = temp->next;
+
+        temp->next = NULL;
+        delete temp;
+        tail = temp;
+    }
+
     void print()
     {
         Node *temp = head;
@@ -59,10 +103,17 @@ int main()
 {
     List ll;
 
-    ll.push_front(3);
-    ll.push_front(2);
-    ll.push_front(1);
+    // ll.push_front(3);
+    // ll.push_front(2);
+    // ll.push_front(1);
     ll.push_front(0);
+
+    ll.push_back(1);
+
+    ll.push_back(2);
+
+    // ll.pop_front();
+    ll.pop_back();
 
     ll.print();
 
