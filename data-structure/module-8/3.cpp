df5d97361249a1719ec2e6fc186811a7188f4f67
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-06 12:43:18 +0600

    Inserting tail in DLL

--- data-structure/module-8/3.cpp
+++ data-structure/module-8/3.cpp
@@ -16,7 +16,7 @@ public:
     }
 };
 
-void insert_at_head(Node *&head, Node *&tail, int val)
+void insert_at_tail(Node *&head, Node *&tail, int val)
 {
     Node *newNode = new Node(val);
 
@@ -26,9 +26,9 @@ void insert_at_head(Node *&head, Node *&tail, int val)
         return;
     }
 
-    newNode->next = head;
-    head->prev = newNode;
-    head = newNode;
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
 }
 
 void print_forward(Node *head)
@@ -42,35 +42,24 @@ void print_forward(Node *head)
     }
 }
 
-void print_backward(Node *tail)
-{
-    Node *temp = tail;
-
-    while (temp != NULL)
-    {
-        cout << temp->val << " ";
-        temp = temp->prev;
-    }
-}
-
 int main()
 {
+    // Node *head = NULL;
+    // Node *tail = NULL;
     Node *head = new Node(10);
     Node *a = new Node(20);
     Node *tail = new Node(30);
 
     head->next = a;
     a->next = tail;
+
     tail->prev = a;
     a->prev = head;
 
-    insert_at_head(head, tail, 5);
-    insert_at_head(head, tail, 0);
+    insert_at_tail(head, tail, 100);
+    insert_at_tail(head, tail, 200);
 
     print_forward(head);
-
-    // cout << endl;
-    // print_backward(tail);
 
     return 0;
 }
