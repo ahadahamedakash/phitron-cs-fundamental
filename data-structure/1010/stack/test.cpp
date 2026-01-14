abf6c8e06d91753bb3ba68c2cf9e94fceecf542a
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-14 23:37:41 +0600

    ...

---
+++ data-structure/1010/stack/test.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.size() == 0)
+                {
+                    return false;
+                }
+
+                if ((st.top() == '(' && s[i] == ')') ||
+                    (st.top() == '{' && s[i] == '}') ||
+                    (st.top() == '[' && s[i] == ']'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return st.size() == 0;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.isValid("()]") << endl;
+
+    return 0;
+}
