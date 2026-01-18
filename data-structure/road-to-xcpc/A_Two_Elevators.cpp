423fc3ef79a4884b570118dc8077879264184eb8
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-18 13:02:04 +0600

    Solved codeforces div3 elevator problem

---
+++ data-structure/road-to-xcpc/A_Two_Elevators.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int timeOne = a - 1;
+        int timeTwo = abs(b - c) + (c - 1);
+
+        if (timeOne < timeTwo)
+            cout << 1 << endl;
+        else if (timeOne > timeTwo)
+            cout << 2 << endl;
+        else
+            cout << 3 << endl;
+    }
+
+    return 0;
+}
