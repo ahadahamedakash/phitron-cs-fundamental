77bbd67ceabd1c1b41fa42a6b13cf0d1cd738a8b
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-18 14:01:15 +0600

    First unique character with array

---
+++ data-structure/1010/queue/5_first_unique_character_in_string.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int firstUniqChar(string s)
+    {
+        int freq[26] = {0};
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        for (int i = 0; i < s.size(); i++)
+            if (freq[s[i] - 'a'] == 1)
+                return i;
+
+        return -1;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    cout << s.firstUniqChar("leetcode") << endl;
+
+    return 0;
+}
