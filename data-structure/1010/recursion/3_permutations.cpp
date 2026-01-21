55ee75f4432155d9db16f758c7d06f5402cde8a6
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-22 01:53:25 +0600

    Permutation using recursion

--- data-structure/1010/recursion/3_permutations.cpp
+++ data-structure/1010/recursion/3_permutations.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+void perms(vector<int> &arr, int idx)
+{
+    if (idx == arr.size())
+    {
+        for (int val : arr)
+            cout << val << " ";
+
+        cout << endl;
+        return;
+    }
+
+    for (int i = idx; i < arr.size(); i++)
+    {
+        swap(arr[idx], arr[i]);
+
+        perms(arr, idx + 1);
+        swap(arr[idx], arr[i]);
+    }
+}
+
 int main()
 {
+    vector<int> arr = {1, 2, 3};
+
+    perms(arr, 0);
 
     return 0;
 }
