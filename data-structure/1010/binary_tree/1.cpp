51ca562fa19797d77991b157c9c3defdb015fa68
ahadahamedakash <a.ahad2232@gmail.com>
2026-01-26 12:30:52 +0600

    Pre, In & Postorder traverse in Binary Tree

--- data-structure/1010/binary_tree/1.cpp
+++ data-structure/1010/binary_tree/1.cpp
@@ -16,9 +16,9 @@ public:
     }
 };
 
-static int idx = -1;
+// static int idx = -1;
 
-Node *build_tree(vector<int> preorder)
+Node *build_tree(vector<int> preorder, int &idx)
 {
     idx++;
 
@@ -27,17 +27,63 @@ Node *build_tree(vector<int> preorder)
 
     Node *root = new Node(preorder[idx]);
 
-    root->left = build_tree(preorder);  // LEFT
-    root->right = build_tree(preorder); // RIGHT
+    root->left = build_tree(preorder, idx);  // LEFT
+    root->right = build_tree(preorder, idx); // RIGHT
 
     return root;
+}
+
+void preorder_traverse(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder_traverse(root->left);
+    preorder_traverse(root->right);
+}
+
+void inorder_traverse(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    inorder_traverse(root->left);
+    cout << root->data << " ";
+    inorder_traverse(root->right);
+}
+
+void postorder_traverste(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    postorder_traverste(root->left);
+    postorder_traverste(root->right);
+    cout << root->data << " ";
 }
 
 int main()
 {
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1}; // root -> left -> right
 
-    Node *root = build_tree(preorder);
+    int idx = -1;
+
+    Node *root = build_tree(preorder, idx);
+
+    cout << "Preorder Traverse: ";
+
+    preorder_traverse(root);
+
+    cout << endl
+         << "Inorder Traverse: ";
+
+    inorder_traverse(root);
+
+    cout << endl
+         << "Postorder Traverse: ";
+
+    postorder_traverste(root);
 
     return 0;
 }
