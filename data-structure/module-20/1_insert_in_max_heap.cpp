a896fefee8e4f0284da4af61b9ebcfe4dfd5f212
ahadahamedakash <a.ahad2232@gmail.com>
2026-02-03 22:46:47 +0600

    Inserting in maximum and minimum heap

---
+++ data-structure/module-20/1_insert_in_max_heap.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    int val;
+    cin >> val;
+    v.push_back(val);
+
+    int current_index = v.size() - 1;
+
+    while (current_index != 0)
+    {
+        int parent_index = (current_index - 1) / 2;
+
+        if (v[parent_index] < v[current_index])
+            swap(v[parent_index], v[current_index]);
+        else
+            break;
+
+        current_index = parent_index;
+    }
+
+    for (int x : v)
+        cout << x << " ";
+
+    return 0;
+}
