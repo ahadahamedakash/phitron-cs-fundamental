7c324ff988456a9d6fced55eeb688a5ca57ca839
ahadahamedakash <a.ahad2232@gmail.com>
2026-02-10 23:12:01 +0600

    Adjacency matrx of undirected graph DS

---
+++ algorithm/module-1/1_adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    int adj_matrix[n][n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         adj_matrix[i][j] = 0;
+    //     }
+    // }
+    memset(adj_matrix, 0, sizeof(adj_matrix)); // make all the value zero
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        adj_matrix[a][b] = 1;
+        adj_matrix[b][a] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
