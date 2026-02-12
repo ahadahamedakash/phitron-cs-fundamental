825265ddcfa1e660dba1fee4cb5b22f34dd1dc82
ahadahamedakash <a.ahad2232@gmail.com>
2026-02-12 23:37:16 +0600

    Path printing

---
+++ algorithm/module-2/4_path_printing.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> adj_list[1005];
+bool visited[1005];
+int level[1005];
+int parent[1005];
+
+void bfs(int src, int dest)
+{
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    level[src] = 0;
+    parent[src] = -1; //
+
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+
+        for (int child : adj_list[par])
+        {
+            if (!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+                level[child] = level[par] + 1;
+                parent[child] = par;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    memset(level, -1, sizeof(level));
+    memset(parent, -1, sizeof(parent));
+
+    int src, dest;
+
+    cin >> src >> dest;
+    bfs(src, dest);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " parent -> " << parent[i] << endl;
+    }
+
+    // cout << level[dest] << endl;
+
+    return 0;
+}
