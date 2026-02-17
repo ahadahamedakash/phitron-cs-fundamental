f0559fdec2cef98470823719e3cc4ec8596d7faa
ahadahamedakash <a.ahad2232@gmail.com>
2026-02-18 00:28:54 +0600

    Detect cycle in Graph

---
+++ algorithm/module-6/1_detect_cycle_in_graph.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cycle;
+bool visited[105];
+vector<int> adj_list[105];
+int parent[105];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+
+    visited[src] = true;
+
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+
+        for (int child : adj_list[par])
+        {
+            if (visited[child] && parent[par] != child)
+                cycle = true;
+
+            if (!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+                parent[child] = par;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    memset(parent, -1, sizeof(parent));
+
+    cycle = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+            bfs(i);
+    }
+
+    if (cycle)
+        cout << "Cycle detected\n";
+    else
+        cout << "No cycle\n";
+
+    return 0;
+}
+
+/*
+INPUT:
+    6 5
+    0 1
+    1 2
+    2 3
+    3 0
+    4 5
+*/
