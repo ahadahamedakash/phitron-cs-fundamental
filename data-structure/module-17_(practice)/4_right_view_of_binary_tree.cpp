014d1ea7d4d1f2be41527cf4fa493fdbcf98c60c
ahadahamedakash <a.ahad2232@gmail.com>
2026-02-28 12:04:35 +0600

    Left and right view of a binary tree

---
+++ data-structure/module-17_(practice)/4_right_view_of_binary_tree.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *btree_input()
+{
+    int val;
+    cin >> val;
+
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+
+    return root;
+}
+
+void right_view(Node *root)
+{
+    if (!root)
+        return;
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        int levelSize = q.size();
+
+        for (int i = 0; i < levelSize; i++)
+        {
+            Node *f = q.front();
+            q.pop();
+
+            if (i == levelSize - 1)
+                cout << f->val << " ";
+
+            if (f->left)
+                q.push(f->left);
+            if (f->right)
+                q.push(f->right);
+        }
+    }
+}
+
+int main()
+{
+    fastIO();
+
+    Node *root = btree_input();
+
+    right_view(root);
+
+    return 0;
+}
