d53d26acd4af9f6d2df55e5823d8621c62f20582
ahadahamedakash <a.ahad2232@gmail.com>
2026-03-03 03:12:30 +0600

    Binary tree paths

---
+++ data-structure/1010/binary_tree/binary_tree_paths.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        left = right = NULL;
+    }
+};
+
+Node *btree_input()
+{
+    int val;
+    cin >> val;
+
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *myLeft, *myRight;
+
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+
+    return root;
+}
+
+void allPaths(Node *root, string path, vector<string> &ans)
+{
+    if (root == NULL)
+        return;
+
+    if (root->left == NULL && root->right == NULL)
+    {
+        ans.push_back(path);
+        return;
+    }
+
+    if (root->left)
+        allPaths(root->left, path + "->" + to_string(root->left->val), ans);
+    if (root->right)
+        allPaths(root->right, path + "->" + to_string(root->right->val), ans);
+}
+
+int main()
+{
+    fastIO();
+
+    Node *root = btree_input();
+
+    vector<string> ans;
+    string path = to_string(root->val);
+
+    allPaths(root, path, ans);
+
+    for (string val : ans)
+    {
+        cout << val << nl;
+    }
+
+    return 0;
+}
+
+/*
+INPUT:
+    1 2 3 -1 -1 4 5 -1 -1 -1 -1
+
+        1
+       / \
+      2   3
+         / \
+        4   5
+*/
