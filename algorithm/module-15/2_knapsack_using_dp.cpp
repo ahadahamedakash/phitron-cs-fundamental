16afe7985094e466eddea9d359484ba5f6c5d8d6
ahadahamedakash <a.ahad2232@gmail.com>
2026-03-15 01:10:11 +0600

    Knapsack using dynamic programming

--- algorithm/module-15/2_knapsack_using_dp.cpp
+++ algorithm/module-15/2_knapsack_using_dp.cpp
@@ -7,22 +7,28 @@
 using namespace std;
 
 int val[1005], weight[1005];
+int dp[1005][1005];
 
 int knapsack(int i, int max_weight)
 {
     if (i < 0 || max_weight <= 0)
         return 0;
 
+    if (dp[i][max_weight] != -1)
+        return dp[i][max_weight];
+
     if (weight[i] <= max_weight)
     {
         int op1 = knapsack(i - 1, max_weight - weight[i]) + val[i];
         int op2 = knapsack(i - 1, max_weight);
 
-        return max(op1, op2);
+        dp[i][max_weight] = max(op1, op2);
+        return dp[i][max_weight];
     }
     else
     {
-        return knapsack(i - 1, max_weight);
+        dp[i][max_weight] = knapsack(i - 1, max_weight);
+        return dp[i][max_weight];
     }
 }
 
@@ -32,6 +38,10 @@ int main()
 
     int n, max_weight;
     cin >> n;
+
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= max_weight; j++)
+            dp[i][j] = -1;
 
     for (int i = 0; i < n; i++)
         cin >> val[i];
