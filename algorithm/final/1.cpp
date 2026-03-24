8ed936bfe2ef03c4d408ac62d84c5e3e3824b000
ahadahamedakash <a.ahad2232@gmail.com>
2026-03-24 17:34:50 +0600

    Solved tetranachi problem using dp(top down)

---
+++ algorithm/final/1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+ll dp[35];
+
+ll tetranachi(ll n)
+{
+    if (n < 2)
+        return n;
+    if (n == 2)
+        return 1;
+    if (n == 3)
+        return 2;
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    dp[n] = tetranachi(n - 1) + tetranachi(n - 2) + tetranachi(n - 3) + tetranachi(n - 4);
+    return dp[n];
+}
+
+int main()
+{
+    fastIO();
+
+    memset(dp, -1, sizeof(dp));
+
+    int n;
+    /*
+        0 <= n <=30
+    */
+    cin >> n;
+    cout << tetranachi(n) << nl;
+
+    return 0;
+}
