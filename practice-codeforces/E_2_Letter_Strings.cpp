ef190857b9bbf74a24ad3db23bf06165f2721f77
ahadahamedakash <a.ahad2232@gmail.com>
2026-05-04 23:26:03 +0600

    Solved week 3 problem I

---
+++ practice-codeforces/E_2_Letter_Strings.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fastIO();
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        ll ans = 0;
+
+        map<char, int> first, last;
+        map<string, int> total;
+        while (n--)
+        {
+            string s;
+            cin >> s;
+            char fs = s[0];
+            char ls = s[1];
+
+            int curPair = first[fs] + last[ls];
+            int extra = 2 * total[s];
+
+            ans += curPair - extra;
+
+            first[fs]++;
+            last[ls]++;
+
+            total[s]++;
+        }
+
+        cout << ans << nl;
+    }
+
+    return 0;
+}
+
+/*
+    LINK: https://codeforces.com/problemset/problem/1669/E
+*/
