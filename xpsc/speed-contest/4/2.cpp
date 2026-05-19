924495734277cfcb12d13cd1613492704218d46f
ahadahamedakash <a.ahad2232@gmail.com>
2026-05-19 22:39:50 +0600

    Solved speed contest 4 - 1 to 5 problems

---
+++ xpsc/speed-contest/4/2.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define nl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fastIO();
+
+    int t;
+    t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        for (int &i : v)
+            cin >> i;
+
+        int chefsMark = v[0], cnt = 0;
+        for (int val : v)
+        {
+            if (val >= chefsMark)
+                cnt++;
+        }
+
+        cout << cnt << nl;
+    }
+
+    return 0;
+}
