3ccc287edd9515cf5d48225aecda41c5daa55f4a
ahadahamedakash <a.ahad2232@gmail.com>
2026-06-01 23:12:09 +0600

    Solved xpsc week 7 - E, CC 206 - D & CF 1669F

--- xpsc/codechef-contest/start226/4.cpp
+++ xpsc/codechef-contest/start226/4.cpp
@@ -10,7 +10,50 @@ using namespace std;
 
 int main()
 {
-    fastIO();
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> v(n);
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> v[i];
+            mp[v[i]]++;
+        }
+
+        int fixed = v[0];
+
+        vector<int> cnt;
+        for (auto &i : mp)
+            if (i.first != fixed)
+                cnt.push_back(i.second);
+
+        sort(all(cnt));
+
+        int distinct = mp.size();
+        int removed = 0;
+
+        for (int val : cnt)
+        {
+            if (k >= val)
+            {
+                k -= val;
+                removed++;
+            }
+            else
+                break;
+        }
+
+        cout << distinct - removed << nl;
+    }
 
     return 0;
 }
