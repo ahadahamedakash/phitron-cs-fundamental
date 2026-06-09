cecabe38167f60edd52e67773ed8a8c4ece6125c
ahadahamedakash <a.ahad2232@gmail.com>
2026-06-10 02:10:51 +0600

    Solved SC 7 Problem 6

--- xpsc/speed-contest/7/5.cpp
+++ xpsc/speed-contest/7/5.cpp
@@ -4,8 +4,6 @@
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define nl '\n'
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 #define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
 using namespace std;
@@ -13,6 +11,40 @@ using namespace std;
 int main()
 {
     fastIO();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        vector<string> grid(n);
+
+        for (int i = 0; i < n; ++i)
+            cin >> grid[i];
+
+        ll ans = 0;
+
+        for (int r = 0; r < n; ++r)
+        {
+            for (int c = 0; c < m; ++c)
+            {
+                int mx = min(n - 1 - r, m - 1 - c);
+
+                for (int d = 1; d <= mx; ++d)
+                {
+                    char ch = grid[r][c];
+
+                    if (grid[r][c + d] == ch && grid[r + d][c] == ch && grid[r + d][c + d] == ch)
+                        ans++;
+                }
+            }
+        }
+
+        cout << ans << nl;
+    }
 
     return 0;
 }
