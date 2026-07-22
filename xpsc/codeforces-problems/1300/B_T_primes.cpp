62203196e6969514008172a2e0ee96aeeb010d6c
ahadahamedakash <a.ahad2232@gmail.com>
2026-07-22 22:31:55 +0600

    Solved CC248C A, B & C SW1 C

---
+++ xpsc/codeforces-problems/1300/B_T_primes.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define spc ' '
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+#define ld long double
+#define ll long long int
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+bool checkRoot(ll n)
+{
+    if (n < 0)
+        return false;
+
+    ll r = sqrtl(n);
+    return (r * r == n);
+}
+
+int main()
+{
+    fastIO();
+
+    int n;
+    cin >> n;
+
+    vector<ll> v(n);
+    for (ll &i : v)
+        cin >> i;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (checkRoot(v[i]))
+        {
+            ll num = sqrtl(v[i]);
+            if (isPrime(num))
+                yes;
+            else
+                no;
+        }
+        else
+        {
+            no;
+        }
+    }
+
+    return 0;
+}
+
+/*
+    https://codeforces.com/problemset/problem/230/B
+*/
