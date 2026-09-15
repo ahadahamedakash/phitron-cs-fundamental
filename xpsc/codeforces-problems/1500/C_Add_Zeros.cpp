1cd4fcf0423447f5f2d58423ab87187a49d5e2fc
ahadahamedakash <a.ahad2232@gmail.com>
2026-09-15 22:18:44 +0600

    Solved CF 2027C

--- xpsc/codeforces-problems/1500/C_Add_Zeros.cpp
+++ xpsc/codeforces-problems/1500/C_Add_Zeros.cpp
@@ -1,109 +1,123 @@
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-// #define nl '\n'
-// #define spc ' '
+#define nl '\n'
+#define spc ' '
 
-// #define imx INT_MAX
-// #define imn INT_MIN
-// #define llmx LLONG_MAX
-// #define llmn LLONG_MIN
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
 
-// #define ll long long int
-// #define ld long double
+#define ll long long int
+#define ld long double
 
-// #define yes cout << "YES\n"
-// #define no cout << "NO\n"
-// #define Yes cout << "Yes\n"
-// #define No cout << "No\n"
-// #define neg1 cout << "-1\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+#define neg1 cout << "-1\n"
 
-// #define sqr(x) ((x) * (x))
-// #define sz(x) ((int)(x).size())
-// #define all(x) (x).begin(), (x).end()
-// #define rall(x) (x).rbegin(), (x).rend()
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 
-// #define dbg(x) cerr << #x << " = " << (x) << nl
+#define dbg(x) cerr << #x << " = " << (x) << nl
 
-// #define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
-// using namespace std;
+using namespace std;
 
-// // Vector
-// using vint = vector<int>;
-// using vll = vector<ll>;
+// Vector
+using vint = vector<int>;
+using vll = vector<ll>;
 
-// // Pair
-// using pii = pair<int, int>;
-// using pll = pair<ll, ll>;
+// Pair
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 
-// // Vector of pairs
-// using vpii = vector<pii>;
-// using vpll = vector<pll>;
+// Vector of pairs
+using vpii = vector<pii>;
+using vpll = vector<pll>;
 
-// // Print vector
-// template <typename T>
-// void printv(const vector<T> &v)
-// {
-//     for (auto x : v)
-//         cout << x << spc;
+// Print vector
+template <typename T>
+void printv(const vector<T> &v)
+{
+    for (auto x : v)
+        cout << x << spc;
 
-//     cout << nl;
-// }
+    cout << nl;
+}
 
-// void smash()
-// {
-//     int n;
-//     cin >> n;
+void smash()
+{
+    int n;
+    cin >> n;
 
-//     vll a(n + 1);
+    unordered_map<ll, vint> mp;
 
-//     vector<pll> v;
+    mp.reserve(2 * n);
+    mp.max_load_factor(0.7);
 
-//     for (int i = 1; i <= n; i++)
-//     {
-//         cin >> a[i];
+    for (int i = 1; i <= n; ++i)
+    {
+        ll x;
+        cin >> x;
 
-//         if (i > 1)
-//         {
-//             ll start = a[i] + i - 1;
-//             ll nxt = start + i - 1;
+        ll need = x + i - 1;
 
-//             v.push_back({start, nxt});
-//         }
-//     }
+        mp[need].push_back(i);
+    }
 
-//     sort(all(v));
+    queue<ll> q;
+    unordered_set<ll> vis;
 
-//     set<ll> reachable;
-//     reachable.insert(n);
+    vis.reserve(2 * n);
+    vis.max_load_factor(0.7);
 
-//     ll ans = n;
+    q.push(n);
+    vis.insert(n);
 
-//     for (auto [start, nxt] : v)
-//     {
-//         if (reachable.count(start))
-//         {
-//             reachable.insert(nxt);
-//             ans = max(ans, nxt);
-//         }
-//     }
+    ll ans = n;
 
-//     cout << ans << nl;
-// }
+    while (!q.empty())
+    {
+        ll len = q.front();
+        q.pop();
 
-// int main()
-// {
-//     fastIO();
+        if (!mp.count(len))
+            continue;
 
-//     int tc;
-//     cin >> tc;
+        for (int i : mp[len])
+        {
+            ll nxt = len + i - 1;
 
-//     while (tc--)
-//         smash();
+            if (!vis.count(nxt))
+            {
+                vis.insert(nxt);
+                q.push(nxt);
+                ans = max(ans, nxt);
+            }
+        }
+    }
 
-//     return 0;
-// }
+    cout << ans << nl;
+}
 
-// /*
-//     https://codeforces.com/problemset/problem/2027/C
-// */
+int main()
+{
+    fastIO();
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+        smash();
+
+    return 0;
+}
+
+/*
+    https://codeforces.com/problemset/problem/2027/C
+*/
