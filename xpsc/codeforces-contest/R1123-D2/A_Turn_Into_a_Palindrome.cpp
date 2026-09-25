7da30ceb21ee90bf864914170246241f649a5d6e
ahadahamedakash <a.ahad2232@gmail.com>
2026-09-25 23:47:48 +0600

    Solved CF R1123 D2 A & B

---
+++ xpsc/codeforces-contest/R1123-D2/A_Turn_Into_a_Palindrome.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+#define nl '\n'
+#define spc ' '
+
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+
+#define ll long long int
+#define ld long double
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+#define neg1 cout << "-1\n"
+
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define dbg(x) cerr << #x << " = " << (x) << nl
+
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+// Vector
+using vint = vector<int>;
+using vll = vector<ll>;
+
+// Pair
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// Vector of pairs
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+// Print vector
+template <typename T>
+void printv(const vector<T> &v)
+{
+    for (auto x : v)
+        cout << x << spc;
+
+    cout << nl;
+}
+
+void smash()
+{
+    int n;
+    char c;
+    cin >> n >> c;
+
+    string s;
+    cin >> s;
+
+    int lt = 0, rt = n - 1, ans = 0;
+    while (lt < rt)
+    {
+        if (s[lt] != s[rt])
+        {
+            if (s[lt] == c || s[rt] == c)
+                ans++;
+            else
+                ans += 2;
+        }
+
+        lt++, rt--;
+    }
+
+    cout << ans << nl;
+}
+
+int main()
+{
+    fastIO();
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+        smash();
+
+    return 0;
+}
---
+++ xpsc/codeforces-contest/R1123-D2/B_Fashionable_Array.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+#define nl '\n'
+#define spc ' '
+
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+
+#define ll long long int
+#define ld long double
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+#define neg1 cout << "-1\n"
+
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define dbg(x) cerr << #x << " = " << (x) << nl
+
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+// Vector
+using vint = vector<int>;
+using vll = vector<ll>;
+
+// Pair
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+// Vector of pairs
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+// Print vector
+template <typename T>
+void printv(const vector<T> &v)
+{
+    for (auto x : v)
+        cout << x << spc;
+
+    cout << nl;
+}
+
+void smash()
+{
+    int n;
+    cin >> n;
+
+    vint v(n);
+    map<int, int> mp;
+    for (auto &i : v)
+    {
+        cin >> i;
+        mp[i]++;
+    }
+
+    vint ans;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (auto it = mp.rbegin(); it != mp.rend(); ++it)
+        {
+            auto [x, y] = *it;
+
+            if (y >= i)
+                ans.push_back(x);
+        }
+    }
+
+    printv(ans);
+}
+
+int main()
+{
+    fastIO();
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+        smash();
+
+    return 0;
+}
